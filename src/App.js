65503ade30ff5fad73658eee46617eb30aac67cf
jethrosweb <jethroldean@gmail.com>
2022-05-03 11:28:16 +1000

    Components functioning, need to style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,83 @@
 import React from "react"
 import "./App.scss"
+import Pad from "./Pad"
+
+const drumBank = [
+    {
+        keyCode: 81,
+        keyTrigger: 'Q',
+        id: 'Chord-1',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    },
+    {
+        keyCode: 87,
+        keyTrigger: 'W',
+        id: 'Chord-2',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    },
+    {
+        keyCode: 69,
+        keyTrigger: 'E',
+        id: 'Chord-3',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    },
+    {
+        keyCode: 65,
+        keyTrigger: 'A',
+        id: 'Shaker',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    },
+    {
+        keyCode: 83,
+        keyTrigger: 'S',
+        id: 'Open-HH',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+    },
+    {
+        keyCode: 68,
+        keyTrigger: 'D',
+        id: 'Closed-HH',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+    },
+    {
+        keyCode: 90,
+        keyTrigger: 'Z',
+        id: 'Punchy-Kick',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+    },
+    {
+        keyCode: 88,
+        keyTrigger: 'X',
+        id: 'Side-Stick',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+    },
+    {
+        keyCode: 67,
+        keyTrigger: 'C',
+        id: 'Snare',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+    }
+];
 
 export default function App() {
+
+    const [volume, setVolume] = React.useState(1)
+
+    const changeVolume = (event) => {
+        setVolume(event.target.value)
+    }
+
     return (
-        <h1>Hello React</h1>
+        <div id="drum-machine">
+            <div className="container">
+                {drumBank.map((clip) => (
+                    <Pad key={clip.id} clip={clip} volume={volume} />
+                ))}
+                <div>
+                    <h4>Volume</h4>
+                    <input type="range" step="0.01" value={volume} max="1" min="0" onChange={changeVolume} />
+                </div>
+            </div>
+        </div>
     )
 }
